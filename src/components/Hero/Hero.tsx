d7a267601bab6ceda79aee7554d18fc80f8b1ca3
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-06-20 15:51:10 +0600

    hero section(right part)

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,17 @@
 "use client";
 import { useEffect, useRef } from "react";
 import localFont from "next/font/local";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import ArticleIcon from '@mui/icons-material/Article';
+import Image from 'next/image'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import ArticleIcon from "@mui/icons-material/Article";
+import img1 from '../../../public/images/img1.jpg'
+import img2 from '../../../public/images/img2.jpg'
+import img3 from '../../../public/images/img3.jpg'
+import img4 from '../../../public/images/img4.jpg'
+import img5 from '../../../public/images/img5.jpg'
+import img6 from '../../../public/images/img6.jpg'
 const myFont = localFont({
   src: [
     {
@@ -75,32 +82,80 @@ export default function Hero() {
           <p className="author-desc">
             And a guy who always likes to explore new technologies, can easily
             cope with the new technology, environment, team and prioritizes user
-            experience most. Currently working with language like javascript, typescript and main frontend tech reactjs, nextjs and backend tech nodejs, expressjs, mongoose
-            and mongodb.
+            experience most. Currently working with language like javascript,
+            typescript and main frontend tech reactjs, nextjs and backend tech
+            nodejs, expressjs, mongoose and mongodb.
           </p>
 
           <div className="socials">
-            <button><GitHubIcon/> <span>GitHub</span></button>
-            <button><LinkedInIcon/> <span>LinkedIn</span></button>
-            <button><EmailIcon/> <span>Email</span></button>
-            <button><ArticleIcon/> <span>Resume</span></button>
+            <button>
+              <GitHubIcon /> <span>GitHub</span>
+            </button>
+            <button>
+              <LinkedInIcon /> <span>LinkedIn</span>
+            </button>
+            <button>
+              <EmailIcon /> <span>Email</span>
+            </button>
+            <button>
+              <ArticleIcon /> <span>Resume</span>
+            </button>
           </div>
         </div>
 
         <div className="right">
-
-
-
-
-
-      
-
-
-
-
-
-
-
+          <div className="scene">
+            <div className="cube">
+              <div className="cube__face cube__face--front">
+                <Image
+                  src={img3}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="cube__face cube__face--back">
+                <Image
+                  src={img1}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="cube__face cube__face--right">
+                <Image
+                  src={img5}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="cube__face cube__face--left">
+                <Image
+                  src={img6}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="cube__face cube__face--top">
+                <Image
+                  src={img2}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="cube__face cube__face--bottom">
+                <Image
+                  src={img4}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
